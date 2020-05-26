9c7fd9a1b6d8cf06e6a139dba77086c06e8d82bd
Michael Phillips <miphilli@redhat.com>
2020-05-26 10:48:55 -0400

    Added comments to my-gsettings.sh

--- files/my-gsettings.sh
+++ files/my-gsettings.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
-gsettings set org.gnome.evolution.calendar use-24hour-format false
+# Use 12-hour clock format instead of 24-hour
+gsettings set org.gnome.desktop.interface clock-format '12h'
+
+# Display seconds in the top-bar clock
 gsettings set org.gnome.desktop.interface clock-show-seconds true
+
+# Enable the gnome-tweaks extension that shows the window list
 gsettings set org.gnome.shell enabled-extensions "['window-list@gnome-shell-extensions.gcampax.github.com']"
+
+# Turn off keyboard repeats
+gsettings set org.gnome.desktop.peripherals.keyboard repeat false
+
