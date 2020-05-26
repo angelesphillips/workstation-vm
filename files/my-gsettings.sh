beb5c5552390a796d0231cad10c346fb9791c5bf
Michael Phillips <miphilli@redhat.com>
2020-05-26 07:32:48 -0700

    First run at customizing workstation VM.

---
+++ files/my-gsettings.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+gsettings set org.gnome.evolution.calendar use-24hour-format false
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.shell enabled-extensions "['window-list@gnome-shell-extensions.gcampax.github.com']"
